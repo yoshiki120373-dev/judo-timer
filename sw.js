57a26064a49d409a0fc540bf2af0ba8567ae4fd0
yoshiki120373-dev <yoshiki120373@gmail.com>
2026-09-11 17:53:52 +0300

    Cache countdown voice files offline

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='judo-timer-v4';
-const CORE=['./manifest.webmanifest','./icon.svg'];
+const CACHE='judo-timer-v5';
+const CORE=['./manifest.webmanifest','./icon.svg','./audio/three.mp3','./audio/two.mp3','./audio/one.mp3'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
